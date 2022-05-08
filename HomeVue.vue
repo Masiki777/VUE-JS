cb30ec80d81fd6490f49039f9e11ba877f1df43b
RAJABU R MASIKI <102743823+Masiki777@users.noreply.github.com>
2022-05-08 16:48:33 +0300

    Add files via upload

---
+++ HomeVue.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>Welcome to Vue js components</h1>
+       <h1>{{data}}</h1>
+       <h1>{{msg}}</h1>
+       
+    </div>
+</template>
+
+<script>
+export default {
+    name:'HomeVue',
+    props:{
+        data:String,
+        msg:String
+    }
+}
+</script>
